ea9cd7e2195a56dcd3c60ab62b4835991ddeaee5
Sam <samarony.barros@gmail.com>
2025-03-22 11:58:44 +0100

    feat: setup environment

---
+++ solution.js
@@ -0,0 +1,5 @@
+function solution(name) {
+  return `Hello, ${name}!!!`;
+}
+
+console.log(solution('World'));
---
+++ solution.ts
@@ -0,0 +1,5 @@
+function solution(name: string): string {
+  return `Hello, ${name}!!!`;
+}
+
+console.log(solution('World'));
